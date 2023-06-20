fccd174ae9c9f90a4484bca25fbdd70eaea4ee83
Malcolm Garner <malcolm.garner4+gitCommitsLocal@gmail.com>
2023-06-20 20:59:03 +0100

    need to set up domain and data fetching from pkbse

---
+++ src/molecules/Login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PocketBase from 'pocketbase';
+
+type Props = {}
+
+async function Login (props: Props) {
+    const pb = new PocketBase('http://');
+
+}
+
+export default Login
